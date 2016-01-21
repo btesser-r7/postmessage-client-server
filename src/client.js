21328f56e2e9ff1d5be7a103fb33926234d1ef3c
Ben Tesser <btesser@rapid7.com>
2016-01-20 21:33:02 -0500

    rapid7 customizations

--- src/client.js
+++ src/client.js
@@ -1,56 +1,82 @@
+'use strict';
+
 /**
  * Creates a client to talk to a server
- * @param target  the url where the server is
- * @param targetWindow  the window where the server is. if null, an iframe will be created
+ * @param {string} target the url where the server is
+ * @param {Window} targetWindow the window where the server is. if null, an iframe will be created
+ * @param {Object.<Function>} methods A dictionary of methods that can be run from the server
  * @returns {promise|*|Q.promise} a promise that will be resolved with the send(method, params) function
  * when the client has connected to the server
  */
-export default function client(target, targetWindow) {
+export default function client(target, targetWindow, methods) {
+  let count = 0,
+      getTargetWindow;
+
   const targetOrigin = target.match(/(.+\/\/[^\/]+)\/?/)[1];
   const promises = [];
-  let getTargetWindow;
-  let count = 0;
   const send = (method, ...args) => {
     const id = count++;
+
     return new Promise((resolve, reject) => {
       promises[id] = {
-        resolve: resolve,
-        reject: reject
+        resolve,
+        reject
       };
       getTargetWindow()
-        .postMessage(JSON.stringify({id: id, postmessageClientServerMethod: method, args: args}), targetOrigin);
+        .postMessage(JSON.stringify({
+          id,
+          args,
+          postmessageClientServerMethod: method
+        }), targetOrigin);
     });
   };
+
   if (targetWindow) {
     getTargetWindow = () => targetWindow;
   } else {
     const iframe = document.createElement('iframe');
+
     iframe.src = target;
     iframe.style.cssText = 'position:absolute;left:-2px;top:-2px;width:1px;height:1px;';
     getTargetWindow = () => iframe.contentWindow;
     document.body.appendChild(iframe);
   }
 
-  return new Promise(resolve => {
-    window.addEventListener('message', event => {
+  return new Promise((resolve) => {
+    window.addEventListener('message', (event) => {
       if (event.source !== getTargetWindow() || event.origin !== targetOrigin) {
         return;
       }
-      const data = JSON.parse(event.data);
-      if (data.postmessageClientServerInit) {
-        // initialized handshake, return send function
-        resolve(send);
-      } else {
-        const callPromise = promises[data.id];
-        if (callPromise) {
-          delete promises[data.id];
-          if (data.error) {
-            callPromise.reject(data.error);
-          } else {
-            callPromise.resolve(data.result);
-          }
+
+      let data;
+
+      try {
+        data = JSON.parse(event.data);
+      } catch (err) {
+        // Unable to parse message from server
+        return;
+      }
+
+      if (data.__postmessage) {
+        if (data.postmessageClientServerInit) {
+          // initialized handshake, return send function
+          resolve(send);
         } else {
-          throw new Error('Cannot find call promise for id ' + data.id);
+          const callPromise = promises[data.id];
+
+          if (callPromise) {
+            delete promises[data.id];
+
+            if (data.error) {
+              callPromise.reject(data.error);
+            } else {
+              callPromise.resolve(data.result);
+            }
+          } else if (methods.hasOwnProperty(data.method)) {
+            methods[data.method](data.result);
+          } else {
+            throw new Error('Cannot find call promise for id ' + data.id);
+          }
         }
       }
     });
--- webpack.config.base.js
+++ webpack.config.base.js
@@ -1,24 +1,14 @@
 'use strict';
 var webpack = require('webpack');
 
-var reactExternal = {
-  root: 'React',
-  commonjs2: 'react',
-  commonjs: 'react',
-  amd: 'react'
-};
-
 module.exports = {
-  externals: {
-    'react': reactExternal
-  },
   module: {
     loaders: [
       { test: /\.js$/, loaders: ['babel-loader'], exclude: /node_modules/ }
     ]
   },
   output: {
-    library: 'ReduxForm',
+    library: 'postmessageClientServer',
     libraryTarget: 'umd'
   },
   resolve: {
